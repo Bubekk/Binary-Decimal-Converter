bc38b7f1408a1d676d53234e006065cbcd31bea5
Bubekk <sebastian.kubanski@gmail.com>
2022-12-01 20:07:48 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Binary-Decimal-Converter/'
-    : '/'
-}
+module.exports = defineConfig({
+  transpileDependencies: true
+})
